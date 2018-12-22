d9265a03f8943b371730be385b9ddb4d4c5af5cd
karldzonson <stribervladimir@gmail.com>
2018-12-22 01:51:38 +0100

    Almost done post and clear messages.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <v-app>
-    <v-toolbar app dark>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>NS</span>
-        <span class="font-weight-light">JS</span>
+    <v-toolbar app class="yellow">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="nsjs_logo headline text-uppercase">
+        <span class="display-1">NS</span>
+        <span class="font-weight-light display-1">JS</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
@@ -36,4 +37,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .nsjs_logo {
+    color: #333;
+  }
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,56 @@
 <template>
- 
+  <v-container class="mainContainer">
+    <h1 class="display-2">Post some question</h1>
+    <form>
+      <input type="text" placeholder="Write some question" class="mainContainer__input" v-model="newQuestion" @keydown.enter.prevent="postQuestion">
+      <v-btn @click="postQuestion">Post question</v-btn>
+    </form>
+    <p>{{ questionContent }}</p>
+    <v-btn @click="clearQuestions">Clear</v-btn>
+  </v-container>
 </template>
 
 <script>
 
 export default {
-  components: {
-    
-  }
+  name: "Home",
+  data() {
+    return {
+      newQuestion: null,
+      questionContent: []
+    }
+  },
+  methods: {
+    postQuestion() {
+      // console.log(this.question)
+      if(this.newQuestion) {
+        this.questionContent.push(this.newQuestion)
+        this.newQuestion = null
+        // console.log(this.questionContent)
+      }
+    },
+    clearQuestions() {
+      this.questionContent = null
+      
+    }
+  }   
 };
 </script>
+
+<style lang="scss" scoped>
+  .mainContainer {
+    form {
+      margin-bottom: 25px;
+    }
+  }
+  h1 {
+    color: #333;
+    margin-bottom: 25px;
+  }
+  .mainContainer__input {
+    height: 200px;
+    width: 100%;
+    font-size: 20px;
+  }
+</style>
+
